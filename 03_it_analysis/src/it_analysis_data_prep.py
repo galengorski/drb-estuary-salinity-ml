5bc962922277bc13510e5b59afde6c726566c7dd
Galen Gorski <32860978+galengorski@users.noreply.github.com>
2021-12-06 14:34:51 -0500

    remove some comments

--- 03_it_analysis/src/it_analysis_data_prep.py
+++ 03_it_analysis/src/it_analysis_data_prep.py
@@ -18,12 +18,6 @@ def download_s3_to_local(s3_dir_prefix, local_outdir, file_id):
     the file names you want to download. For example 'usgs_nwis' will 
     download all files with 'usgs_nwis' in the file name'''
     
-    # after you have configured saml2aws, you can log in and create a new 
-    # token by executing the following command:
-    # keep all of the s3 arguments hard coded as I don't see us changing them much
-    # subprocess.run(["saml2aws", "login", "--skip-prompt", "--role", "arn:aws:iam::807615458658:role/adfs-wma-developer"])
-    
-    # read in S3 credentials from ./.aws/credentials file
     # assumes we are using a credential profile names 'dev'
     write_location = 'local'
     aws_profile = 'dev'
