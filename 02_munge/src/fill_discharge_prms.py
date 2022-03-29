7e198f6466b65967622cc8989d2895f33116fc4f
Galen Gorski <32860978+galengorski@users.noreply.github.com>
2022-03-29 14:57:04 -0400

    Apply suggestions from code review
    looks good
    
    Co-authored-by: Amelia <30877272+amsnyder@users.noreply.github.com>

--- 02_munge/src/fill_discharge_prms.py
+++ 02_munge/src/fill_discharge_prms.py
@@ -28,37 +28,29 @@ def download_unzip_sb(sb_url, prms_predictions, destination):
         
 
 
-def fill_discharge_prms(trenton_seg_id_nat, schuylkill_seg_id_nat, nwis_trenton_location, nwis_schuylkill_location, destination, prms_predictions):
+def fill_discharge_prms(fill_segments, destination, prms_predictions):
     '''reads in prms predictions of discharge and fills nan gaps for trenton and schuylkill nwis time series
-    :trenton_seg_id_nat: [int] segment id closest to trenton discharge location
-    :schuylkill_seg_id_nat: [int] segment id closest to schylkill discharge location
-    :nwis_trenton_location: [str] location of munged trenton nwis .csv
-    :nwis_schuylkill_location: [str] location of munged schuylkill nwis .csv
+    :fill_segments: [dict] dictionary of sites to fill data for; each site in the dictionary contains 'nwis_site' (nwis site number) and 'seg_id_nat' (segment id closest to discharge location)
     :destination: [str] directory where prms data file is located
     :prms_predictions: [str] name of prms file'''
     
     sn_temp_data = pd.read_csv(os.path.join(destination,prms_predictions+'.csv'), parse_dates = True, index_col = 'date')
     
-    sn_temp_trenton = sn_temp_data[sn_temp_data['seg_id_nat'] == trenton_seg_id_nat]
-    sn_temp_schuylkill = sn_temp_data[sn_temp_data['seg_id_nat'] == schuylkill_seg_id_nat]
-    
-    nwis_trenton = pd.read_csv(nwis_trenton_location, parse_dates = True, index_col = 'datetime')
-    nwis_schuylkill = pd.read_csv(nwis_schuylkill_location, parse_dates = True, index_col = 'datetime')
-    
+for segment_details in fill_segments.values():
+    sn_temp_site = sn_temp_data[sn_temp_data['seg_id_nat'] == segment_details['seg_id_nat']]
+
+    nwis_data_path = os.path.join('02_munge', 'out', 'D', 'usgs_nwis_{}.csv'.format(segment_details['nwis_site']))
+    nwis_site_data = pd.read_csv(nwis_data_path, parse_dates = True, index_col = 'datetime')
+
     #fill the gaps make sure to from cubic meters to cfs
-    nwis_trenton['filled'] = nwis_trenton['discharge'].fillna(sn_temp_trenton['seg_outflow']*35.315)
-    nwis_schuylkill['filled'] = nwis_schuylkill['discharge'].fillna(sn_temp_schuylkill['seg_outflow']*35.315)
-    
+    nwis_site_data['filled'] = nwis_site_data['discharge'].fillna(sn_temp_site['seg_outflow']*35.315)
+
     #drop the old discharge and rename filled as discharge
-    nwis_trenton = nwis_trenton.drop(columns = 'discharge')
-    nwis_trenton['discharge'] = nwis_trenton['filled'].rename('discharge')
-    
-    nwis_schuylkill = nwis_schuylkill.drop(columns = 'discharge')
-    nwis_schuylkill['discharge'] = nwis_schuylkill['filled'].rename('discharge')
-    
+    nwis_site_data = nwis_site_data.drop(columns = 'discharge')
+    nwis_site_data.rename(columns = {'filled':'discharge'}, inplace=True)
+
     #write to dataframe
-    nwis_trenton.to_csv(nwis_trenton_location)
-    nwis_schuylkill.to_csv(nwis_schuylkill_location)
+    nwis_site_data.to_csv(nwis_data_path)
 
 
 def main():
@@ -69,13 +61,10 @@ def main():
     sb_url = config['sb_url']
     prms_predictions = config['prms_predictions']
     destination = config['destination']
-    trenton_seg_id_nat = config['trenton_seg_id_nat']
-    schuylkill_seg_id_nat = config['schuylkill_seg_id_nat']
-    nwis_trenton_location = config['nwis_trenton_location']
-    nwis_schuylkill_location = config['nwis_schuylkill_location']
+    fill_segments = config['fill_segments']
     
     download_unzip_sb(sb_url, prms_predictions, destination)
-    fill_discharge_prms(trenton_seg_id_nat, schuylkill_seg_id_nat, nwis_trenton_location, nwis_schuylkill_location, destination, prms_predictions)
+    fill_discharge_prms(fill_segments, destination, prms_predictions)
 
 if __name__ == '__main__':
     main()
