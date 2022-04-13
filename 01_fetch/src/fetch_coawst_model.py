69836643f0caac88c9aab05b75ee31a326fdc8a1
Thompson <tthompson@usgs.gov>
2022-04-13 10:58:24 -0400

    modify fetch COAWST model script

--- 01_fetch/src/fetch_coawst_model.py
+++ 01_fetch/src/fetch_coawst_model.py
@@ -8,17 +8,16 @@ from dask.distributed import Client
 client = Client()
 client
 
-def fetch_COAWST_model_run(url):
+def load_COAWST_model_run(url):
     ds = xr.open_dataset(url, chunks={'ocean_time':720})
     ds = xr.Dataset(ds, coords={'lon': (['eta_rho', 'xi_rho'], nc['lon_rho']),
                           'lat': (['eta_rho', 'xi_rho'], nc['lat_rho']),
                           's': nc['s_rho'])
     print(f'Size: {ds.nbytes / (-10**9)} GB')
-    u = url.split('/')
-    print(u[12])
+    print(run_number)
     return ds
                                 
-def river_mile_timeseries():
+def salt_front_timeseries(write_location, s3_client, s3_bucket, run_number):
     # read river mile coordinates csv
     river_mile_coords = pd.read_csv(river_mile_coords_filepath, index_col=0)
     
@@ -31,35 +30,34 @@ def river_mile_timeseries():
     dist_mile = xr.DataArray(dist_mile,dims=["points"]) 
     
     # select variable for timeseries along shore
-    ds = ds[variable].isel(xi_rho=ds.lon,eta_rho=ds.lat) 
+    ds = ds.isel(xi_rho=target_x,eta_rho=target_y) 
     
     # assign river mile distance as a new coordinate in dataset
     ds = ds.assign_coords({'dist_mile': dist_mile})
     
     # sort by river mile, subset values from 1st river mile
-    salt = ds.sortby(ds.dist_mile).isel(s_rho=0).where(salt>0.5)
+    salt = ds.isel(s_rho=0).sortby(ds.dist_mile)
     
-    # select salt variable
-    salt = salt.salt
-    print(f'Size: {salt.nbytes / (-10**9)} GB')
+    #locate saltfront
+    saltfront = salt.where(salt < 0.52).where(salt > 0.5)
+    saltfront_location = saltfront.where(saltfront.max('ocean_time'))
     
-    # create new netcdf with subsetted data
-    if write_location == 'S3':
-        try: 
-            s3.Object(s3_bucket, netcdf_path).load()
-        except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] == "404":
-                salt.to_netcdf(netcdf_path)
-                print('uploading to s3')
-                s3_client.upload_file(s3_bucket, '01_fetch/out/' + netcdf_filename)
-    else:
-        if os.path.isfile(netcdf_path):
-            print ("File exist")
-        else:
-        salt.to_netcdf(netcdf_path)
-        
-    # close original salt dataset; improves performance
-    salt.close()
+    # convert Datarray to dataframe
+    saltfront_location= saltfront_location.to_dataframe()
+    
+    # tidy dataframe
+    df = saltfront_location[saltfront_location['salt'].notna()]
+    df = df.droplevel(level=1)
+    
+    # take daily average
+    df = df.resample('1D').mean()
+    
+    saltfront_data = os.path.join('.', '01_fetch', 'out', f'salt_front_location_from_COAWST_run_{run_number}.csv')
+    df.to_csv(saltfront_data, index=False)
+    # upload csv with salt front data to S3
+        if write_location == 'S3':
+        print('uploading to s3')
+        s3_client.upload_file(saltfront_data, s3_bucket, '01_fetch/out/'+os.path.basename(saltfront_data))
                                 
 def main():
     # import config
@@ -70,13 +68,15 @@ def main():
     write_location = config['write_location']
     s3_client = utils.prep_write_location(write_location, config['aws_profile'])
     s3_bucket = config['s3_bucket']
-        
+    
+    # define model run
     url = config['url']
+    u = url.split('/')
+    run_number = u[12]
     
+    # define csv with river mile coordinates
     river_mile_coords_filepath = config['river_mile_coords_filepath']
-    variable = config['variable']
-    
-    netcdf_filename = f"coawst_salt_{model_run}_{model_run_year}.nc"
-    netcdf_path = os.path.join('.', '01_fetch', 'out', filename)
 if __name__ == '__main__':
-    main()
+    load_COAWST_model_run()
+    salt_front_timeseries()
+    
