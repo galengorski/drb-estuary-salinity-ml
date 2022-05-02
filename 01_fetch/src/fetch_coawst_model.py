88441b55c49bcf4fda0a4b1ad020cb9c7d4613fe
Amelia <30877272+amsnyder@users.noreply.github.com>
2022-05-02 17:50:58 -0400

    update naming

--- 01_fetch/src/fetch_coawst_model.py
+++ 01_fetch/src/fetch_coawst_model.py
@@ -66,10 +66,10 @@ def salt_front_timeseries(ds, river_mile_coords_filepath, run_number):
     saltfront = salt.where(salt.salt < 0.53)
 
     # subset salt variable
-    saltfront_location_salt = saltfront_location.salt
+    saltfront_location = saltfront_location.salt
 
     # convert to dataframe
-    df = saltfront_location_salt.to_dataframe()
+    df = saltfront_location.to_dataframe()
 
     # drop points index column so we only have one index (ocean_time)
     df = df.droplevel(level=1)
