2871ff9b8f2abf32b0f6d7a5be4f3a92f6b8735f
Amelia <30877272+amsnyder@users.noreply.github.com>
2022-05-02 16:50:09 -0400

    add additional commenting to coawst model fetch script

--- 01_fetch/src/fetch_coawst_model.py
+++ 01_fetch/src/fetch_coawst_model.py
@@ -8,8 +8,8 @@ from dask.distributed import Client
 import yaml
 import utils
 
-client = Client()
-client
+#client = Client()
+#client
 
 # import config
 with open("01_fetch/params_config_fetch_coawst_model.yaml", 'r') as stream:
@@ -37,45 +37,54 @@ def load_COAWST_model_run(url):
     return ds
 
 def salt_front_timeseries(ds, river_mile_coords_filepath, run_number):
-    # read river mile coordinates csv
+    # read river mile coordinates csv, which contains geospatial
+    # information about the locations want to read data from
+    # in the COAWST model
     river_mile_coords = pd.read_csv(river_mile_coords_filepath, index_col=0)
 
-    # create array of river miles as points
+    # read in netcdf indices that we will pull data from
     target_x = np.array(river_mile_coords.iloc[:,[1]].values).squeeze()
-    target_x = xr.DataArray(target_x-1, dims=["dist_miles"]) 
+    target_x = xr.DataArray(target_x-1, dims=["points"]) 
     target_y = np.array(river_mile_coords.iloc[:,[2]].values).squeeze()
-    target_y = xr.DataArray(target_y-1,dims=["dist_miles"])
+    target_y = xr.DataArray(target_y-1,dims=["points"])
+    # read in the corresponding river mile location for each point
     dist_mile = np.array(river_mile_coords.iloc[:,[0]].values).squeeze()
     dist_mile = xr.DataArray(dist_mile,dims=["points"])
 
-    # sort by river mile, subset values from 1st river mile
+    # pull the bottom-most vertical layer of the river profile
+    # ranges from 0-16, with 0 being the bottom
     salt = ds.isel(s_rho=0)
 
-    # select variable for timeseries along shore
+    # select data points from pathway along shore
+    # these points were provided by the COAWST modeling team
     salt = salt.isel(xi_rho=target_x,eta_rho=target_y)
 
     # assign river mile distance as a new coordinate in dataset
     salt = salt.assign_coords({'dist_mile': dist_mile})
 
-    #locate saltfront
+    # locate saltfront
     saltfront = salt.where(salt.salt < 0.53).where(salt.salt > 0.5)
     saltfront_location = saltfront.where(saltfront.max('ocean_time'))
 
     # subset salt variable
-    saltfront_location = saltfront_location.salt
+    saltfront_location_salt = saltfront_location.salt
 
-    #convert to dataframe
-    df = saltfront_location.to_dataframe()
+    # convert to dataframe
+    df = saltfront_location_salt.to_dataframe()
 
     # tidy dataframe
-    df = df[df['salt'].notna()]
-    df = df.droplevel(level=1)
+    # get location of salt front at each hour of the day
+    df_short = df[df['salt'].notna()]
+    
+    # drop points index column so we only have one index (ocean_time)
+    df_drop = df_short.droplevel(level=1)
 
-    # take daily average
-    df = df.resample('1D').mean()
+    # take daily average by averaging hourly location throughout day 
+    df_mean = df_drop.resample('1D').mean()
 
     saltfront_data = os.path.join('.', '01_fetch', 'out', f'salt_front_location_from_COAWST_run_{run_number}.csv')
-    df.to_csv(saltfront_data, index=False)
+    df_mean.to_csv(saltfront_data, index=False)
+
     # upload csv with salt front data to S3
     if write_location == 'S3':
         print('uploading to s3')
